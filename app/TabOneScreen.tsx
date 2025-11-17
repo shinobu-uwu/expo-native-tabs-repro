7993beef2717afd93cf6f807b1b6ab494ad0ca02
Matheus Filipe dos Santos Reinert <mreinert@segura.security>
2025-11-17 11:47:02 -0300

    repro

---
+++ app/TabOneScreen.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import { styles } from '.';
+
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+    </View>
+  );
+}
+
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  Icon,
+  Label,
+  NativeTabs,
+  VectorIcon,
+} from 'expo-router/unstable-native-tabs';
+import { FontAwesome6 } from '@expo/vector-icons';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+export default function() {
+  return (<NativeTabs>
+    <NativeTabs.Trigger name="index">
+      <Label>One</Label>
+      <Icon src={{
+        default: <VectorIcon family={FontAwesome6} name="key" />,
+        selected: <VectorIcon family={FontAwesome6} name="gear" />,
+      }} />
+    </NativeTabs.Trigger>
+    <NativeTabs.Trigger name="two">
+      <Label>Two</Label>
+      <Icon src={{
+        default: <VectorIcon family={FontAwesome6} name="key" />,
+        selected: <VectorIcon family={FontAwesome6} name="gear" />,
+      }} />
+    </NativeTabs.Trigger>
+  </NativeTabs>)
 }
